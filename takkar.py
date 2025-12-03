a7e056c443cf27f6f3d8179aee15cb7a127578cf
Ansourbey <ansourgashutuhar7@gmail.com>
2025-12-03 10:39:37 +0300

    Add files via upload

---
+++ takkar.py
@@ -0,0 +1,8 @@
+a = int(input(print ("enter size of trump's button: ")))
+b = int(input(print ("enter size of kim's button: ")))
+if a > b :
+    print ("MEGA")
+elif a < b :
+    print ("FAKE NEWS!")
+else :
+    print ("WORLD WAR 3!")
